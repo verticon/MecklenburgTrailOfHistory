0eefc9d91e9c34c9f7eb60dfb95b8ca55c5edd8e
Robert Vaessen <robert@rvaessen.com>
2017-08-29 10:37:36 -0400

    Finished the Detail View. It could use more polishing (the shadow, the opaque background) but is "good enough".

--- Trail of History/Detail View/DetailView.swift
+++ Trail of History/Detail View/DetailView.swift
@@ -12,10 +12,15 @@ class DetailView : UIView {
 
     private let textView = UITextView()
     private let imageView = UIImageView()
-    private let modalViewController = UIViewController()
+    private let controller = UIViewController()
+    private let presenter: UIViewController
 
+    private let inset: CGFloat = 16
 
     public init(poi: PointOfInterest) {
+        let window = UIApplication.shared.delegate!.window!!
+        presenter = window.visibleViewController!
+
         super.init(frame: CGRect.zero)
 
         backgroundColor = UIColor(white: 1, alpha: 0.5)
@@ -23,16 +28,18 @@ class DetailView : UIView {
         
         // AFAIK If no other action is taken then the image view will size itself to the image, even if this results in a size larger than the window.
         imageView.image = poi.image
-        imageView.contentMode = .scaleAspectFit
+        imageView.contentMode = poi.image.size.width > (window.frame.width - 2*inset) || poi.image.size.height > (window.frame.height - 2*inset) ? .scaleAspectFit : .scaleAspectFill
         imageView.translatesAutoresizingMaskIntoConstraints = false
         
         addSubview(imageView)
 
+        /*
         let textAttributes = [NSStrokeColorAttributeName : UIColor.black,
                               NSForegroundColorAttributeName : UIColor.white,
                               NSStrokeWidthAttributeName : -3.0] as [String : Any]
         textView.attributedText = NSAttributedString(string: poi.description, attributes: textAttributes)
-        textView.backgroundColor = .clear
+        */
+        textView.text = poi.description
         textView.isEditable = false
         textView.font = UIFont.init(name: "Helvetica", size: 18)
         textView.translatesAutoresizingMaskIntoConstraints = false
@@ -41,15 +48,20 @@ class DetailView : UIView {
 
         // Here we constrain the image view to not exceed the size of its superview; this causes the scaleAspectFit contentMode to "kick in".
         NSLayoutConstraint.activate([
-            imageView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
             imageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
-            imageView.widthAnchor.constraint(lessThanOrEqualTo: self.widthAnchor, multiplier: 1.0),
-            imageView.heightAnchor.constraint(lessThanOrEqualTo: self.heightAnchor, multiplier: 1.0),
+            imageView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: inset),
+            imageView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -inset),
+            imageView.heightAnchor.constraint(lessThanOrEqualTo: self.heightAnchor),
+
+            textView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            textView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            textView.widthAnchor.constraint(equalTo: imageView.widthAnchor),
+            textView.heightAnchor.constraint(equalTo: imageView.heightAnchor),
             ])
 
-        modalViewController.modalPresentationStyle = .overCurrentContext
-        modalViewController.modalTransitionStyle = .crossDissolve
-        modalViewController.view = self
+        controller.modalPresentationStyle = .overCurrentContext
+        controller.modalTransitionStyle = .crossDissolve
+        controller.view = self
     }
 
     required public init?(coder aDecoder: NSCoder) {
@@ -58,17 +70,68 @@ class DetailView : UIView {
 
     public override func layoutSubviews() {
         super.layoutSubviews()
-        
-        textView.center = center // Hmmmm ...
+ 
+        let imageSize = aspectFitImageSize()
+        let barHeight = presenter.navigationController?.navigationBar.frame.maxY ?? UIApplication.shared.statusBarFrame.height
+        let availableVerticalSpace = self.frame.height - (barHeight + 2*inset)
 
-        // At this point the image view's size will have been set and wee can use it to siz the text view.
-        textView.bounds.size = aspectFitImageSize()
+        func info(_ header: String) {
+            print("\n\(header)")
+            print("\tRoot View: \(self.frame)")
+            print("\tImage View: \(imageView.frame)")
+            print("\tText View: \(textView.frame)")
+            print("\tImage Size: \(imageSize)")
+            print("\tText Content Size: \(textView.contentSize)")
+        }
+
+        //info("Before")
+
+
+        // What we want to accomplish is that the image (which might be shorter that the image view) sits on top
+        // of the text view and that the space above the image (not counting the bars) be equal to the space below
+        // the text view. If there is a sufficient amount of text then the above/below spaces will go to almost zero
+        // (we will enforce an inset) and scrolling of the text will "kick in"
+        //
+        // Note: The code assumes that the image and text view are initially centered in their super view.
+        // This should have be assurred by the contraints that were applied by the initializer.
+
+        
+        var newTextFrame = textView.frame
+        newTextFrame.size.height = textView.contentSize.height
+        if newTextFrame.height + imageSize.height > availableVerticalSpace {
+            newTextFrame.size.height = availableVerticalSpace - imageSize.height
+        }
+        textView.frame = newTextFrame
+        
+        let totalHeight = imageSize.height + textView.frame.height
+
+        let currImageTop = self.center.y - imageSize.height/2
+        let newImageTop = self.center.y - totalHeight/2 + barHeight/2
+        imageView.frame.origin.y -= currImageTop - newImageTop
+
+        let currTextTop = textView.frame.origin.y
+        let newTextTop = newImageTop + imageSize.height
+        textView.frame.origin.y -= currTextTop - newTextTop
+ 
+        let shadowView = UIView(frame: CGRect(x: inset, y: newImageTop, width: imageSize.width, height: totalHeight))
+        let radius: CGFloat = shadowView.frame.width / 2.0 //change it to .height if you need spread for height
+        let shadowPath = UIBezierPath(rect: CGRect(x: 0, y: 0, width: 2.1 * radius, height: shadowView.frame.height))
+        //Change 2.1 to amount of spread you need and for height replace the code for height
+        shadowView.layer.cornerRadius = 2
+        shadowView.layer.shadowColor = UIColor.black.cgColor
+        shadowView.layer.shadowOffset = CGSize(width: 0.5, height: 0.4)  //Here you control x and y
+        shadowView.layer.shadowOpacity = 0.5
+        shadowView.layer.shadowRadius = 5.0 //Here your control your blur
+        shadowView.layer.masksToBounds =  false
+        shadowView.layer.shadowPath = shadowPath.cgPath
+        self.insertSubview(shadowView, at: 0)
+
+        info("After")
+        print("\tShadow View: \(shadowView.frame)")
     }
 
     func present() {
-        let window = UIApplication.shared.delegate!.window!!
-        let presentingViewController = window.visibleViewController!
-        presentingViewController.present(modalViewController, animated: false, completion: nil)
+        presenter.present(controller, animated: false, completion: nil)
     }
     
     private func aspectFitImageSize() -> CGSize {
@@ -83,7 +146,7 @@ class DetailView : UIView {
 
     @objc private func dismiss(_ sender: UITapGestureRecognizer) {
         if sender.state == .ended {
-            modalViewController.dismiss(animated: false, completion: nil)
+            controller.dismiss(animated: false, completion: nil)
         }
     }
     
