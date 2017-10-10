0773f1184b58c9f5904e1337e65221401f9cfae1
Robert Vaessen <robert@rvaessen.com>
2017-10-10 11:23:22 -0400

    VerticonsToolbox.UserLocation no longer requires an explicit enable.

--- Trail of History/AppDelegate.swift
+++ Trail of History/AppDelegate.swift
@@ -17,14 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         print("\(applicationName) started")
         
         FIRApp.configure()
         FIRDatabase.database().persistenceEnabled = true
-        
-        UserLocation.enable()
 
         return true
     }
